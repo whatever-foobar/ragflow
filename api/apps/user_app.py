a6ea22f78e63167361da76bf5f7aa02f13afdab3
whateverer <196654242@qq.com>
2025-04-17 09:43:40 +0800

    fix: multi user login

--- api/apps/user_app.py
+++ api/apps/user_app.py
@@ -99,7 +99,7 @@ def login():
     user = UserService.query_user(email, password)
     if user:
         response_data = user.to_json()
-        user.access_token = get_uuid()
+        # user.access_token = get_uuid()
         login_user(user)
         user.update_time = (current_timestamp(),)
         user.update_date = (datetime_format(datetime.now()),)
@@ -466,7 +466,7 @@ def log_out():
         schema:
           type: object
     """
-    current_user.access_token = f"INVALID_{secrets.token_hex(16)}"
+    # current_user.access_token = f"INVALID_{secrets.token_hex(16)}"
     current_user.save()
     logout_user()
     return get_json_result(data=True)
@@ -514,6 +514,8 @@ def setting_user():
             )
 
         if new_password:
+            current_user.access_token = get_uuid() # patch
+            current_user.save() # patch
             update_dict["password"] = generate_password_hash(decrypt(new_password))
 
     for k in request_data.keys():
