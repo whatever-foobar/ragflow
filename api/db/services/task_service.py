7d8ac04d8d057c006331e8ce9d2a2203f2295e94
whateverer <196654242@qq.com>
2025-04-23 20:29:54 +0800

    change: effectively remove retry limit

--- api/db/services/task_service.py
+++ api/db/services/task_service.py
@@ -120,8 +120,8 @@ class TaskService(CommonService):
 
         msg = f"\n{datetime.now().strftime('%H:%M:%S')} Task has been received."
         prog = random.random() / 10.0
-        if docs[0]["retry_count"] >= 3:
-            msg = "\nERROR: Task is abandoned after 3 times attempts."
+        if docs[0]["retry_count"] >= 2048:
+            msg = "\nERROR: Task is abandoned after 2048 times attempts."
             prog = -1
 
         cls.model.update(
@@ -130,7 +130,7 @@ class TaskService(CommonService):
             retry_count=docs[0]["retry_count"] + 1,
         ).where(cls.model.id == docs[0]["id"]).execute()
 
-        if docs[0]["retry_count"] >= 3:
+        if docs[0]["retry_count"] >= 2048:
             return None
 
         return docs[0]
